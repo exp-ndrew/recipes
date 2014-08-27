72c02eaf407a62d4f375a2524c4f1a9798307d50
Matt Waechter & Andrew M Westling <student@epicodus.com>
2014-08-27 11:27:48 -0700

    add sass styles

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,11 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+    render('recipes/index.html.erb')
+  end
+
+  def new
+    @recipe = Recipe.new
+    render('recipes/new.html.erb')
+  end
+end
